81cfa56a99d41989aca0f718b75342d5a6354ab5
Sabah Sabrina Ashraf <sabahsabrina@yahoo.com>
2022-04-03 17:46:57 -0700

    dashboard created

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,117 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Dashboard.css";
+import {
+  Area,
+  AreaChart,
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Scatter,
+  ScatterChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+  ZAxis,
+} from "recharts";
 
 const Dashboard = () => {
+  const [profit, setProfit] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProfit(data);
+      });
+  }, []);
   return (
-    <div>
-      <h2>This is dashboard page</h2>
+    <div className="chart-container">
+      <div className="line-chart">
+        <h3>Monthwise Sell</h3>
+        <LineChart
+          width={450}
+          height={250}
+          data={profit}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis dataKey="sell" />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+        </LineChart>
+      </div>
+      <div className="area-chart">
+        <h3>Investment VS Revenue</h3>
+        <AreaChart
+          width={450}
+          height={250}
+          data={profit}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="month" />
+          <YAxis dataKey="investment" />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="investment"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+          <Area
+            type="monotone"
+            dataKey="revenue"
+            stroke="#82ca9d"
+            fillOpacity={1}
+            fill="url(#colorPv)"
+          />
+        </AreaChart>
+      </div>
+      <div className="bar-chart">
+        <h3>Investment VS Revenue</h3>
+        <BarChart width={450} height={250} data={profit}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis dataKey="investment" />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="investment" fill="#8884d8" />
+          <Bar dataKey="revenue" fill="#82ca9d" />
+        </BarChart>
+      </div>
+      <div className="scattered-chart">
+        <h3>Investment VS Revenue</h3>
+        <ScatterChart
+          width={450}
+          height={250}
+          margin={{ top: 20, right: 20, bottom: 10, left: 10 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" name="stature" />
+          <YAxis dataKey="sell" />
+          <ZAxis dataKey="revenue" range={[64, 144]} />
+          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+          <Legend />
+          <Scatter name="sell" data={profit} fill="#8884d8" />
+          <Scatter name="revenue" data={profit} fill="#82ca9d" />
+        </ScatterChart>
+      </div>
     </div>
   );
 };
